4470cc1ce17ac9b710a9328199b231dde892bced
jonchang03 <jonchang03@gmail.com>
2016-11-21 22:25:13 -0500

    Scraping HTML Data with Beautiful Soup

--- PythonWebData/urllink3.py
+++ PythonWebData/urllink3.py
@@ -8,19 +8,28 @@
 # Actual data: http://python-data.dr-chuck.net/comments_197759.html (Sum ends with 51)
 import urllib.request
 from bs4 import BeautifulSoup
+import re
 
 url = input('Enter - ')
-if len(url) < 1 : url = "http://python-data.dr-chuck.net/comments_42.html"
+if len(url) < 1 : url = "http://python-data.dr-chuck.net/comments_197759.html"
 html = urllib.request.urlopen(url).read()
 
-soup = BeautifulSoup(html)
+soup = BeautifulSoup(html, "lxml")
 
 
 # Retrieve all of the span tags
 tags = soup('span')
+# empty list to add
+numberList = list()
 for tag in tags:
     # Look at the parts of a tag
-    print('TAG:',tag)
-    print('URL:',tag.get('href', None))
-    print('Contents:',tag.contents[0])
-    print('Attrs:',tag.attrs)
+    # print('TAG:',tag)
+    # print('URL:',tag.get('href', None))
+    # print('Contents:',tag.contents[0])
+    # print('Attrs:',tag.attrs)
+    numberList += re.findall('^[0-9]+$', tag.contents[0]) # findall returns a list of the one number per line
+
+# conver list of strings to list of ints using list comprehension
+numberList = [int(i) for i in numberList]
+print(numberList)
+print(sum(numberList))
